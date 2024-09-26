9ebab1d2fba833cc17c5bfb1ecea41bced7afb07
alexaidana077 <maidanaalex077@gmail.com>
2024-09-26 13:00:14 -0300

    correcciones

--- paginas php/php/registro.php
+++ paginas php/php/registro.php
@@ -17,18 +17,18 @@ if ($conn->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recibir los datos del formulario
-    $gmail = $_POST['gmail'];
+    $gmail = $_POST['email'];
     $contraseña = $_POST['contraseña'];
     $usuario = isset($_POST['usuario']) ? $_POST['usuario'] : NULL; // verifica si se ingreso algun valor o si no se ingresa NULL
 
     //se verifica si se ingresaron valores en gmail y contraseña
-    if (!empty($gmail) && !empty($contraseña)) {
+    if (!empty($email) && !empty($contraseña)) {
         
         //En esta parte del codigo se hashea la contraseña, el PASSWORD_DEFAULT sirve para asegurarc que se utilice el metodo mas seguro que este disponible
         $contraseña_hashed = password_hash($contraseña, PASSWORD_DEFAULT); 
 
         //se insertan los datos en la tabla de usuarios
-        $sql = "INSERT INTO usuarios (gmail, contraseña, usuario) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO usuarios (email, contraseña, usuario) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("sss", $gmail, $contraseña_hashed, $usuario);
 
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Registro exitoso";
         } else{
         if ($conn -> errno = 1062){
-            echo "Error: Correro ya utilizado";
+            echo "Error: El correo ya existe";
         }else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
